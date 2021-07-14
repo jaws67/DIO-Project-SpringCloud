5d2fcf0226f8e71c739751aa9a83a18fe170ef25
jaws67 <alexmendes132@gmail.com>
2021-07-13 22:28:39 -0300

    first commit

---
+++ product-catalog/src/main/java/com/dio/productcatalog/config/ElasticSearchConfig.java
@@ -0,0 +1,24 @@
+package com.dio.productcatalog.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories (basePackages = "com.dio.live3.productcatalog.repository")
+public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
+
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder() //
+                .connectedTo("localhost:9200","localhost:9300") //
+                .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+}
---
+++ service-discovrey/src/test/java/com/dio/servicediscovrey/ServiceDiscovreyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.servicediscovrey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscovreyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
